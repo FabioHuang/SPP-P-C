1d997fb5dbc48339381094450dfed46e1113f34e
Fabio Huang <fabio_huang@ufms.br>
2026-01-20 15:44:41 -0400

    feat: adding padding in ImageHandler

--- utils/image_handler.py
+++ utils/image_handler.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageOps
 from PIL.Image import Image as PImage
 from math import ceil
 from pathlib import Path
@@ -9,27 +9,26 @@ class ImageHandler():
     def __init__(self):
         pass
 
-    def crop_image(self, img: PImage, grid_resolution: float) -> PImage:
+    def crop_image(self, img: PImage, grid_resolution: float, padding: float = 0) -> PImage:
         ''' Crops and adds padding to images to fit in a grid with defined resolution '''
         # Ensure transparency layer
         if img.mode not in ('RGBA', 'LA'):
             img = img.convert('RGBA')
 
         bbox = img.getbbox()
-        img = img.crop(bbox)
         
-        # TODO Add padding...
+        img = img.crop(bbox)
+        img = ImageOps.expand(img, border=padding)
+
         w = bbox[2] - bbox[0]
         h = bbox[3] - bbox[1]
         new_w = ceil((w) / grid_resolution) * grid_resolution
         new_h = ceil((h) / grid_resolution) * grid_resolution
         
         grid_img = Image.new('RGBA', (new_w, new_h))
-        print(img.size, grid_img.size)
+
         grid_img.paste(img, ((new_w - w) // 2, (new_h - h) // 2))
         
-        grid_img.show()
-
         return grid_img
 
     def get_image(self, img_path: Path) -> PImage:
@@ -46,13 +45,3 @@ class ImageHandler():
     
     def get_size(self, img: PImage) -> Tuple[int, int]:
         return img.size
-
-if __name__ == "__main__":
-    car_path = Path("/home/ventilas/Pictures/car.png")
-    resolution = 100
-    handler = ImageHandler()
-    img = handler.get_image(car_path)
-    grid_img = handler.crop_image(img, resolution)
-    grid_img.filename = "new_car.png"
-    save_path = Path("/home/ventilas/Pictures/")
-    handler.save_image(grid_img, save_path)
