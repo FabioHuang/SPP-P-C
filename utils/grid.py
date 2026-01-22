1f3bf507dd4f39d1614c6cddb366ae18e4f4d4df
Fabio Huang <fabio_huang@ufms.br>
2026-01-22 15:24:12 -0400

    fix: using image handler in grid class

--- utils/grid.py
+++ utils/grid.py
@@ -1,13 +1,19 @@
+from image_handler import ImageHandler
+
 from PIL import Image
 from PIL.Image import Image as PImage
-from uuid import uuid64
+from uuid import uuid4
+from pathlib import Path
 
 from typing import List
+
 
 class Grid():
     def __init__(self, width: float, resolution: float):
         self.W = width
         self.resolution = resolution
+
+        self.handler = ImageHandler()
 
         self.items = {}
 
@@ -16,22 +22,27 @@ class Grid():
         
         optimal_grid = Image.new("RGBA", (self.W, H * self.resolution))
         
-        for item in self.items:
-            optimal_x, optimal_y = optimal_placement[item["id"]]
+        for id, item in self.items.items():
+            optimal_x, optimal_y = optimal_placement[id]
             optimal_grid.paste(item["image"], (optimal_x * self.resolution, optimal_y * resolution))
 
         return optimal_grid 
 
-    def add(self, img: PImage) -> None:
+    def add(self, img: PImage | Path) -> None:
+        if img.isinstance(Path):
+            img = self.handler.get_image(img)
+        
+        img = self.handler.crop(img)
+
         # Sanity check
-        assert img.size[0] % resolution == 0
-        assert img.size[1] % resolution == 0
+        assert img.size[0] % self.resolution == 0
+        assert img.size[1] % self.resolution == 0
         assert img.mode in ("RGBA", "LA")
 
-        id = str(uuid64())
+        id = str(uuid4())
         w, h = img.size
 
-        self.items[id] = {'w': w % resolution, 'h': h % resolution, "image": img}
+        self.items[id] = {'w': w // self.resolution, 'h': h // self.resolution, "image": img}
 
         return None
 
